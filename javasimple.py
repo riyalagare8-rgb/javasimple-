300adfe84ca76c658f6a26f9487cb6278687138e
riyalagare8-rgb <riyalagare8@gmail.com>
2025-11-11 18:39:54 +0530

    Refactor simple interest calculation with user input

--- javasimple.py
+++ javasimple.py
@@ -1,7 +1,19 @@
-principal = 10000 
-rate = 5           
-time = 3           
-
-simple_interest = (principal * rate * time) / 100
-
-print("Simple Interest is:", simple_interest)
+import sys
+if len(sys.argv) ==4:
+    script_name=sys.argv[0]
+    principle=sys.argv[1]
+    rate=sys.argv[2]
+    time=sys.argv[3]
+    print("user provided inputs")
+else:
+    script_name=sys.argv[0]
+    principle="1000"
+    rate="5"
+    time="3"
+    print("default inputs")
+si=(int(principle)*float(rate)*int(time))/100
+print("Script Name:",script_name)   
+print("Principle Amount:",principle)
+print("Rate of Interest:",rate)
+print("Time (years):",time)
+print("Simple Interest:",si)
